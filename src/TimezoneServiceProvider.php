87ac9e97590d06036352bd48da8b783951050519
dungdt <dungdt@rikkeisoft.vn>
2016-01-25 16:24:59 +0700

    First laravel package

---
+++ src/TimezoneServiceProvider.php
@@ -0,0 +1,28 @@
+<?php namespace Laraveldaily\Timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezoneServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+        {
+            $this->loadViewsFrom(__DIR__.'/views', 'timezones');
+        }
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+        {
+            include __DIR__.'/routes.php';
+            $this->app->make('Laraveldaily\Timezones\TimezonesController');
+        }
+
+}
